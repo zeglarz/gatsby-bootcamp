6d313a4e8101fb1e578566ee23c2069c6c5412e1
Konrad Rudnicki <zeglarz@gmail.com>
2020-03-24 20:07:15 +0100

    improvements to individual blog page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,34 +2,35 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql, useStaticQuery } from "gatsby"
 
-const Blog = ({ location }) => {
-  const data = useStaticQuery(graphql`
-      query {
-          allMarkdownRemark {
-              edges {
-                  node {
-                      frontmatter {
-                          title
-                          date
-                      }
-                      html
-                      excerpt
-                      fields {
-                          slug
-                      }
-                  }
-              }
-          }
-      }
-  `)
+export const query = graphql`
+    query (
+        $slug: String!
+    ){
+        markdownRemark (
+            fields: {
+                slug: {
+                    eq: $slug
+                }
+            }
+        )
+        {
+            frontmatter {
+                title
+                date
+            }
+            html
+        }
+    }
+`
 
-  const currPost = data.allMarkdownRemark.edges.find(post => post.node.fields.slug === location.pathname.split("/")[2])
-  console.log(currPost)
+const Blog = ({ data: { markdownRemark: { frontmatter: { title, date }, html } } }) => {
+
+
   return (
     <Layout>
-      <h1>{currPost.node.frontmatter.title}</h1>
-      <p>{currPost.node.frontmatter.date}</p>
-      <p>{currPost.node.excerpt}</p>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }}/>
 
     </Layout>
   )
